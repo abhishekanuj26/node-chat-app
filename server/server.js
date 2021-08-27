d27518c80f1295558ff17646739ed3d97be0ab15
Abhishek Anuj <26abhishekanuj@gmail.com>
2021-08-27 16:09:12 +0530

    added socket connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const path=require('path');
 const express=require('express');
-
+const http=require('http');
+const socketIo=require('socket.io');
 
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
 var app=express();
+var server=http.createServer(app);
+var io=socketIo(server);
 app.use(express.static(publicPath));
-app.listen(port,(req,res)=>{
+
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+
+    socket.on('disconnect',()=>{
+        console.log('client disconnected');
+    });
+});
+server.listen(port,(req,res)=>{
     console.log('server is running on port 3000');
 });
