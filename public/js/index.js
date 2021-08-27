9e3f2173820c1127bb9ae5b716c6765c02b53b35
Abhishek Anuj <26abhishekanuj@gmail.com>
2021-08-27 23:49:36 +0530

    added form for chatting

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 var socket=io();
 socket.on('connect',function(){
     console.log('connected to the server');
@@ -8,4 +9,20 @@ socket.on('disconnect',function(){
 });
 socket.on('newMessage',function(message){
     console.log('new message',message);
+
+    var li=jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+    
+});
+
+jQuery('#m-form').on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:"User",
+        text:jQuery('[name=message]').val()
+    },function(){
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path=require('path');
 const express=require('express');
 const http=require('http');
 const socketIo=require('socket.io');
+
 const {messageGenerator}=require('./utils/message')
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
