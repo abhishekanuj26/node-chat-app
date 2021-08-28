0cfc318dae91211a78410009868cb1b2f7b47d88
Abhishek Anuj <26abhishekanuj@gmail.com>
2021-08-28 22:54:13 +0530

    dynamic texts included

---
+++ playground/time.js
@@ -0,0 +1,3 @@
+var moment=require('moment');
+var time=moment();
+console.log(time.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
-
 var socket=io();
 socket.on('connect',function(){
     console.log('connected to the server');
@@ -8,21 +7,41 @@ socket.on('disconnect',function(){
     console.log('disconnected');
 });
 socket.on('newMessage',function(message){
+
+    var template = jQuery('#message-template').html();
+    var html=Mustache.render(template,{
+        text:message.text,
+        from:message.from
+    });
+
+  jQuery('#messages').append(html);
+    /* 
     console.log('new message',message);
 
     var li=jQuery('<li></li>');
     li.text(`${message.from}: ${message.text}`);
     jQuery('#messages').append(li);
+    */
     
 });
 
 socket.on('newLocationMessage',function(message){
+
+    var temp=jQuery('#location_message-template').html();
+    var html=Mustache.render(temp,{
+        from:message.from,
+        url:message.url
+    });
+    jQuery('#messages').append(html);
+
+    /*
     var li=jQuery('<li></li>');
     var a=jQuery('<a target="_blank">my current location</a>');
     li.text(`${message.from}: `);
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li);
+    */
 
 });
 
