04963575ca0eb7032bdc314441d4eb155c96342b
Abhishek Anuj <26abhishekanuj@gmail.com>
2021-08-29 14:25:36 +0530

    added user name to send text and location

--- server/server.js
+++ server/server.js
@@ -38,13 +38,21 @@ io.on('connection',(socket)=>{
 
     socket.on('createMessage',(message,callback)=>{
         console.log('new message',message);
-        io.emit('newMessage',messageGenerator(message.from,message.text));
+        var user=users.getuser(socket.id);
+       
+        if(user && isrealstr(message.text)){
+            io.to(user.room).emit('newMessage',messageGenerator(user.name,message.text));
+        }
+        
        callback();
     });
 
 
     socket.on('createLocation',(coords)=>{
-        io.emit('newLocationMessage',locationGenerator('admin',coords.latitude,coords.longitude));
+        var user=users.getuser(socket.id);
+        if(user){
+        io.to(user.room).emit('newLocationMessage',locationGenerator(user.name,coords.latitude,coords.longitude));
+        }
     })
     socket.on('disconnect',(params)=>{
         console.log('client disconnected');
